a2e316d5b909c542df0c674cb043727af9ce844d
Divanshu Chauhan <23524935+Divkix@users.noreply.github.com>
2022-04-05 11:57:02 -0700

    Initialize app

---
+++ README.md
@@ -0,0 +1,16 @@
+# hcaptcha-verifier
+
+A simple API to verify hcaptcha tokens.
+
+## Deploy your own
+
+[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2FDivideProjects%2Fhcaptcha-verifier&demo-title=hCaptcha%20Verifier&demo-description=A%20simple%20API%20to%20verify%20hCaptcha%20tokens.&demo-url=https%3A%2F%2Fhcaptcha-verifier.divkix.me?utm_source=divideprojects&utm_campaign=oss)
+
+
+## Usage
+Docs for usage are placed here:
+
+https://hcaptcha-verifier.divkix.me
+
+
+[![Sponsor](https://www.datocms-assets.com/31049/1618983297-powered-by-vercel.svg)](https://vercel.com/?utm_source=divideprojects&utm_campaign=oss)
---
+++ app.py
@@ -0,0 +1,56 @@
+from os import getcwd
+from typing import Optional
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from prettyconf import Configuration
+from prettyconf.loaders import EnvFile, Environment
+from pydantic import BaseModel
+from requests import post
+
+# Load configuration
+env_file = f"{getcwd()}/.env"
+config = Configuration(loaders=[Environment(), EnvFile(filename=env_file)])
+
+# define the app
+app = FastAPI()
+
+# add cors policy to avoid CORS errors
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="*",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# get the secret for account from ENV
+HCAPTCHA_SECRET = config(
+    "HCAPTCHA_SECRET",
+    cast=str,
+    default="0x0000000000000000000000000000000000000000",
+)
+HCAPTCHA_URL = "https://hcaptcha.com/siteverify"
+
+# class of the model for verifying the captcha
+class Verification(BaseModel):
+    token: str
+    secret_key: Optional[str] = HCAPTCHA_SECRET
+
+
+# Data to show when the user visits the verify url
+@app.post("/verify")
+def read_item(v: Verification):
+    dict_data = v.dict()
+    data = {
+        "response": dict_data["token"],
+        "secret": dict_data["secret_key"],
+    }
+    r = post("https://hcaptcha.com/siteverify", data=data)
+    return r.json()
+
+
+# Data to show when the user visits the homepage
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
